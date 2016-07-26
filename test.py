3653e50d763bc72f94c1012077bdc70a30747a04
AntonyPallups <antony.pallupetta@gmail.com>
2016-07-26 19:20:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,23 @@
 import difflib
-s1=[1,8,3,9,4,9,3,8,1,2,3]
-s2=[1,8,1,3,9,4,9,3,8,1,2,3]
-sm=difflib.SequenceMatcher(None,s1,s2)
-print sm.ratio()
+
+#data fetch part
+#data from AccuWeather for Kochi with center as 18 Jul
+pastWeek=[30,30,31,30,31,31,31]
+pastYearFortnyt=[32,32,32,32,29,31,28,30,27,29,29,28,30,30,30]
+
+start = range(len(pastYearFortnyt) + 1)
+stop = start[7:]
+L = [pastYearFortnyt[i:j] for i, j in zip(start, stop)]
+
+similarity = 0
+
+for 
+sm = difflib.SequenceMatcher(None,pastWeek,L1)
+similarity = sm.ratio()
+
+sm=difflib.SequenceMatcher(None,pastWeek,L2)
+if sm.ratio()>similarity:
+	similarity = sm.ratio()
+else
+
+
